d8ff82f9e2577fec843956dedab8526cbafdd941
zovits <zovits@roblox.com>
2025-12-18 15:08:11 -0800

    Mock apply should do something visually

--- plugin/src/Main.server.lua
+++ plugin/src/Main.server.lua
@@ -1,4 +1,4 @@
 local DeltaManager = require(script.Parent.DeltaManager)
 
-task.wait(4) -- Wait for Studio to finish loading content and rendering
+task.wait(5) -- Wait for Studio to finish loading content and rendering
 DeltaManager.init()
